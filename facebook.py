9b28435367e7b735ada8fb641bfdbeeb09a94be2
celine21-meet <52948684+celine21-meet@users.noreply.github.com>
2019-11-20 13:02:37 -0500

    Add files via upload

--- facebook.py
+++ facebook.py
@@ -16,18 +16,36 @@ class User(object):
 	def post(self,text):
 		self.posts.append(text)
 		print(self.name+" has posted "+ text)
+		post1 = Post("post", self.name)
 	def get_userinfo(self):
 		print("Name:"+self.name)
 		print("Email:"+self.email)
 		print("Password:"+self.password)
 		print("Friends:"+str(self.friends_list))
 		print("Posts:"+str(self.posts))
+
+class Post(object):
+	def __init__(self,name,caption,comments=[]):
+		self.name = name
+		self.caption = caption
+		self.comments = []
+	def add_caption(self,caption_text):
+		self.caption_text = caption_text
+		self.caption.append(caption_text)
+		print(self.name + " has added the caption " + caption_text)
+	def add_comment(self, comment_text):
+		self.comment_text = comment_text
+		self.comment.append(comment_text)
+		print(self.name + " has added a comment " + comment_text)
+	def like_post(self):
+		print(self.name + " has liked your post!")
 user1 = User("Layan","layann.hamdan@gmail.com","LaYan2019",0,0)
 user2 = User("Hiba","Hiba22@gmail.com","Hibaaa9102",0,0)
 user1.add_friend(user2.email)
 user1.post("Hi")
 user1.get_userinfo()
 user1.remove_friend(user2.email)
-class Post(object):
-	def __init__(self,)
+user1.post("a new post")
+
+
 
